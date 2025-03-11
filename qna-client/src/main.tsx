94d26227bbaa418d0f8be881831cfddd989394ee
Jeong Hwan Lee <Ju2cyjerry@gmail.com>
2025-03-11 23:57:01 +0900

    fix: ensure type safety in main.tsx

--- qna-client/src/main.tsx
+++ qna-client/src/main.tsx
@@ -5,14 +5,30 @@ import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { LoadingProvider } from "./components/LoadingSpinner";
 
+const rootElement = document.getElementById("root");
+
 // 둘 다 가능
 // createRoot(document.getElementById("root")!).render(
-createRoot(document.getElementById("root") as HTMLElement).render(
-  <StrictMode>
-    <LoadingProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </LoadingProvider>
-  </StrictMode>
-);
+
+if (rootElement) {
+  createRoot(rootElement).render(
+    <StrictMode>
+      <LoadingProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </LoadingProvider>
+    </StrictMode>
+  );
+} else {
+  console.error("Root Element not found");
+}
+// createRoot(document.getElementById("root") as HTMLElement).render(
+//   <StrictMode>
+//     <LoadingProvider>
+//       <BrowserRouter>
+//         <App />
+//       </BrowserRouter>
+//     </LoadingProvider>
+//   </StrictMode>
+// );
